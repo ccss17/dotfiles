6e03fb38bf3139c8fa4bbacd4ce54ea72d599326
ccsss <chansol0505@naver.com>
2024-09-08 01:07:00 +0900

    lsd-rs

--- install.sh
+++ install.sh
@@ -32,8 +32,8 @@ case "$distro" in
     fi
     if ! type lsd 2>/dev/null; then
         DEBFILE="lsd.deb"
-        VERSION=`curl -s https://api.github.com/repos/Peltoche/lsd/releases/latest | grep tag_name | cut -d '"' -f 4`
-        wget -q -O $DEBFILE https://github.com/Peltoche/lsd/releases/download/$VERSION/lsd_${VERSION}_amd64.deb
+        VERSION=`curl -s https://api.github.com/repos/lsd-rs/lsd/releases/latest | grep tag_name | cut -d '"' -f 4`
+        wget -q -O $DEBFILE https://github.com/lsd-rs/lsd/releases/download/$VERSION/lsd_${VERSION}_amd64.deb
         $SUDO dpkg -i $DEBFILE
     fi
     if ! type hexyl 2>/dev/null; then
