0595dc53c4d1e18b6cca1ded3b6c6dc229af3a2b
ccsss <chansol0505@naver.com>
2025-01-21 21:22:13 +0900

    omp

---
+++ vscode.ps1
@@ -0,0 +1,41 @@
+# ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+If (Test-Path "~\miniconda3\Scripts\conda.exe") {
+    (& "~\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+}
+
+function clearls { clear ; ls }
+function cdup { cd .. }
+function exitf { exit }
+function lsa { ls -a }
+function lsla { ls -la }
+function lstree { ls --tree }
+function get_ip {
+  $IpAddress = (Get-NetIPAddress |
+    Where-Object {
+      $_.AddressState -eq 'Preferred' -and 
+      $_.ValidLifetime -lt '24:00:00'
+    }
+  );$IpAddress.IPAddress
+}
+function get_public_ip {
+  (Invoke-WebRequest ifconfig.me/ip).Content.Trim()
+}
+
+# New-Alias d docker
+# New-Alias c clear
+# New-Alias g git
+# New-Alias q exitf
+# Remove-Alias ls
+# New-Alias ls lsd
+# New-Alias l ls
+# New-Alias la lsa
+# New-Alias ll lsla
+# New-Alias lt lstree
+# New-Alias cl clearls
+# New-Alias cs cdup
+# New-Alias ip get_ip
+
+$Env:ip = get_ip
+$Env:public_ip = get_public_ip
+
+oh-my-posh init pwsh --config ~\repo\dotfiles\paradox_custom_left.omp.json | Invoke-Expression
